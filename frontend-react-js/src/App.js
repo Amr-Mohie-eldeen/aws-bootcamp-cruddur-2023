138217385d4df0be9a43e0d024b9286094a82b7e
Amr-Mohie-eldeen <amr.mohie@hotmail.com>
2023-02-21 22:02:15 +0000

    edited open api file

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -9,7 +9,8 @@ import MessageGroupsPage from './pages/MessageGroupsPage';
 import MessageGroupPage from './pages/MessageGroupPage';
 import ConfirmationPage from './pages/ConfirmationPage';
 import React from 'react';
-import process from 'process';
+import NotificationsFeedPage from './pages/NotificationsFeedPage';
+
 import {
   createBrowserRouter,
   RouterProvider
@@ -47,6 +48,10 @@ const router = createBrowserRouter([
   {
     path: "/forgot",
     element: <RecoverPage />
+  },
+  {
+    path: "/notifications",
+    element: <NotificationsFeedPage />
   }
 ]);
 
