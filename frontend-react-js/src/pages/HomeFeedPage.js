0ac6cda802e64f4caaaae18fc60b79d427b998cb
Amr-Mohie-eldeen <amr.mohie@hotmail.com>
2023-03-19 20:54:43 +0000

    Dynamic handle with rds

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -23,9 +23,6 @@ export default function HomeFeedPage() {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
       const res = await fetch(backend_url, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`
-        },
         method: "GET"
       });
       let resJson = await res.json();
@@ -75,7 +72,8 @@ const checkAuth = async () => {
         <ActivityForm  
           popped={popped}
           setPopped={setPopped} 
-          setActivities={setActivities} 
+          setActivities={setActivities}
+          user={user}
         />
         <ReplyForm 
           activity={replyActivity} 
