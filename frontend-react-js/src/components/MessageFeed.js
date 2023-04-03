83d55c98690997b1e545437de66f6a9856701379
Amr-Mohie-eldeen <amr.mohie@hotmail.com>
2023-04-03 00:57:40 +0000

    implemented dm auto scroll to last mes

--- frontend-react-js/src/components/MessageFeed.js
+++ frontend-react-js/src/components/MessageFeed.js
@@ -1,15 +1,34 @@
-import './MessageFeed.css';
-import MessageItem from './MessageItem';
+import React, { useEffect, useRef } from "react";
+import "./MessageFeed.css";
+import MessageItem from "./MessageItem";
 
 export default function MessageFeed(props) {
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "auto" });
+    }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [props.messages]);
+
   return (
-    <div className='message_feed'>
-      <div className='message_feed_heading'>
-        <div className='title'>Messages</div>
+    <div className="message_feed">
+      <div className="message_feed_heading">
+        <div className="title">Messages</div>
       </div>
-      <div className='message_feed_collection'>
-        {props.messages.map(message => {
-        return  <MessageItem key={message.uuid} message={message} />
+      <div className="message_feed_collection">
+        {props.messages.map((message, index) => {
+          return (
+            <MessageItem
+              key={message.uuid}
+              message={message}
+              ref={index === props.messages.length - 1 ? messagesEndRef : null}
+            />
+          );
         })}
       </div>
     </div>
